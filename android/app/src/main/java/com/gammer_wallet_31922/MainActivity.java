5a82657b009399f689567d591f0cf9bf982c7984
Crowdbotics <team@crowdbotics.com>
2021-11-11 13:05:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gammer_wallet_31922/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gammer_wallet_31922;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gammer_wallet_31922";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gammer_wallet_31922",
+  url: "https://gammer_wallet_31922.botics.co",
+  api: "https://gammer_wallet_31922.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
